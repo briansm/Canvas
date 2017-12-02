9be5f87a90a52c6c64b900cc9c5b161f77766dad
Brian Morales <briansmor09@gmail.com>
2017-12-01 20:45:08 -0600

    Dibujo con canvas

--- Dibujo.js
+++ Dibujo.js
@@ -1,1 +1,22 @@
-alert("to play");
+var d=document.getElementById("candib");
+var lienzo=d.getContext("2d");
+var lineas=30;
+var xf,yi;
+var colorcito="#FAA";
+
+for(l=0;l<lineas;l++){
+  yi=10*l;
+  xf=10*(l+1)
+  dibujarline(colorcito,0,yi,xf,300);
+}
+dibujarline(colorcito,1,1,1,299);
+dibujarline(colorcito,1,299,299,299);
+
+function dibujarline(color,xini,xfin,yini,yfin){
+  lienzo.beginPath();
+  lienzo.strokeStyle=color;
+  lienzo.moveTo(xini,yini);
+  lienzo.lineTo(xfin,yfin);
+  lienzo.stroke();
+  lienzo.closePath();
+}
