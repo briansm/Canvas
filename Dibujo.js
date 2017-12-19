b976e38a828c8357b769301f2ce2219b089272b7
Brian Morales <briansmor09@gmail.com>
2017-12-19 12:37:18 -0600

    Dibujando en canvas

--- Dibujo.js
+++ Dibujo.js
@@ -2,6 +2,7 @@ var texto=document.getElementById("txtlineas");
 var btn=document.getElementById("boton");
 btn.addEventListener("click",dibujoPorclick);
 var d=document.getElementById("candib");
+var ancho=d.width;
 var lienzo=d.getContext("2d");
 
 function dibujarline(color,xini,xfin,yini,yfin){
@@ -14,14 +15,13 @@ function dibujarline(color,xini,xfin,yini,yfin){
 }
 
 function dibujoPorclick(){
-  var cat=parseInt(texto.value);
-  var lineas=cat;
+  var lineas=parseInt(texto.value);
   var xf,yi;
   var colorcito="#FAA";
-
+  var espacio=ancho/lineas;
   for(l=0;l<lineas;l++){
-    yi=10*l;
-    xf=10*(l+1)
+    yi=espacio*l;
+    xf=espacio*(l+1)
     dibujarline(colorcito,0,yi,xf,300);
   }
   dibujarline(colorcito,1,1,1,299);
